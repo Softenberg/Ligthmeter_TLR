de089fd37cedc529fa11edc39f97b351507921ab
Andreas <93anan@gmail.com>
2023-02-08 15:41:33 +0100

    changed more screen stuff next is major ovarhaul?

--- src/main.cpp
+++ src/main.cpp
@@ -17,11 +17,11 @@
 #define ROTARY_BUTTON_GPIO 17
 
 #define VALID_APERTURES_SIZE 12
-#define VALID_SHUTTERS_SIZE 9
+#define VALID_SHUTTERS_SIZE 10
 #define VALID_ISOS_SIZE 9
 
 #define OLED_COL_WIDTH 35 
-
+uint cycle = 1;
 
 bool DEBUG = false;
 
@@ -41,7 +41,7 @@ PIO pio = pio0;
 
 static const int valid_apertures[VALID_APERTURES_SIZE] = {95,  140, 200,  280,  350,  400,
                                                           560, 800, 1100, 1600, 2200, 3200};
-static const int valid_shutters[VALID_SHUTTERS_SIZE] = {2, 4, 10, 20, 40, 100, 200, 500, 1000};
+static const int valid_shutters[VALID_SHUTTERS_SIZE] = {1, 2, 4, 10, 20, 40, 100, 200, 500, 1000};
 static const int valid_isos[VALID_ISOS_SIZE] = {50, 100, 200, 400, 800, 1600, 3200, 6400, 12800};
 
 
@@ -125,6 +125,12 @@ void display_erase(String text, int col, int line)
     display.display();
 }
 
+void display_erase_area(int col, int line)
+{
+    display.fillRect(col, line, 128, 8, BLACK);
+    //display.display();
+}
+
 void setup_display()
 {
     if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
@@ -142,11 +148,12 @@ void setup_display()
     display_print(mode_text[CameraMode::Shutter] + ":", 0, 16);
     display_print(mode_text[CameraMode::ISO] + ":", 0, 0);
     display_print("MODE:", 0, 24);
-    display_print(String(valid_apertures[exposure.aperture]/10.0), OLED_COL_WIDTH, 8);
+    display_print("APT", 85, 0);
+    display_print("SHU", 85, 16);
+    display_print(String(valid_apertures[exposure.aperture]/100.0), OLED_COL_WIDTH, 8);
     display_print("1/"+String(valid_shutters[exposure.shutter]), OLED_COL_WIDTH, 16);
     display_print(String(valid_isos[exposure.iso]), OLED_COL_WIDTH, 0);
     display_print(mode_text[mode.current], OLED_COL_WIDTH, 24);
-
     display.display();
 }
 
@@ -242,11 +249,10 @@ double calculate_apature()
     double shu = valid_shutters[exposure.shutter];
     double apt = 1.0/shu*valid_isos[exposure.iso]*pow(2, EV)/100.0;
     Serial.print(apt);
-    apt = round(apt*10)/10.0;
+    apt = round(apt*10)/100.0;
     Serial.print(apt);
     return apt;
 }
-
 
 void display_text(int encoder)
 {
@@ -259,8 +265,8 @@ void display_text(int encoder)
     }
     if (exposure.aperture != exposure.prev.aperture)
     {
-        display_erase(String(valid_apertures[exposure.prev.aperture]/10.0), OLED_COL_WIDTH, 8);
-        display_print(String(valid_apertures[exposure.aperture]/10.0), OLED_COL_WIDTH, 8);
+        display_erase(String(valid_apertures[exposure.prev.aperture]/100.0), OLED_COL_WIDTH, 8);
+        display_print(String(valid_apertures[exposure.aperture]/100.0), OLED_COL_WIDTH, 8);
         exposure.prev.aperture = exposure.aperture;
     }
     if (exposure.shutter != exposure.prev.shutter)
@@ -289,17 +295,18 @@ void display_calculated_shutter(int calculated_shutter)
 {
     if (calculated_shutter != old_calculated_shutter)
     {   
-        display_erase("1/"+String(old_calculated_shutter), 70, 16);
-        display_print("1/"+String(calculated_shutter), 70, 16);
+        display_erase_area(80, 24);
+        display_print("1/"+String(calculated_shutter), 80, 24);
         old_calculated_shutter = calculated_shutter;
     }
 }
+
 void display_calculated_apature(double calculated_apature)
 {
     if (calculated_apature != old_calculated_apature)
     {   
-        display_erase(String(old_calculated_apature), 70, 8);
-        display_print(String(calculated_apature), 70, 8);
+        display_erase_area(75, 8);
+        display_print("f/"+String(calculated_apature), 75, 8);
         old_calculated_apature = calculated_apature;
     }
 }
@@ -346,14 +353,18 @@ void loop()
 
         display_text(encoder);
     }
-    if (mode.current == 0 || mode.current == 2)
-    {
-            display_calculated_shutter(calculate_shutter());
+
+    if(cycle % 25 == 0){
+        if (mode.current == 0 || mode.current == 2)
+        {
+                display_calculated_shutter(calculate_shutter());
+        }
+        if (mode.current == 1 || mode.current == 2)
+        {
+                display_calculated_apature(calculate_apature());
+        }
     }
-    if (mode.current == 1 || mode.current == 2)
-    {
-            display_calculated_apature(calculate_apature());
-    }
-    delay(50);
+    cycle ++;
+    delay(30);
     //Serial.print(String(exposure.aperture) + " " + String(exposure.shutter) + " " + String(exposure.iso) + "---");
 }
